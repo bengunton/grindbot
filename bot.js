db63b604aba105ee9218df53850ef12c45699acb
Ben Gunton <bengunton@btinternet.com>
2022-03-25 18:38:07 +0000

    ping pong

--- bot.js
+++ bot.js
@@ -5,7 +5,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);

074a86079bedf2cd20a3162e98de1be21915fcef
Ben Gunton <bengunton@btinternet.com>
2022-03-25 19:54:21 +0000

    add start mmr command

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ const commands = {
     ping: "ping",
     win: "win",
     loss: "loss",
-    startMmr: "startMmr",
+    startMmr: "startmmr",
 }
     
 client.on('message', msg => {
@@ -38,10 +38,35 @@ client.on('message', msg => {
         losses++;
         replyWinLoss(msg);
     }
+
+    if (messageContent.startsWith(commands.startMmr)) {
+        replyToStartMmr(msg);
+    }
 });
 
 replyWinLoss = (msg) => {
     msg.reply(`Wins: ${wins}, Losses: ${losses}`)
+}
+
+var mmr = 0;
+var replyId = 0;
+
+replyToStartMmr = (msg) => {
+    content = msg.content.slice(commands.startMmr.length + prefix.length);
+    startingValue = parseInt(content);
+
+    if (isNaN(startingValue)) {
+        console.log('invalid starting mmr:', content)
+        msg.reply('Not a valid mmr to start on');
+        return;
+    }
+
+    mmr = startingValue;
+    msg.reply(`MMR: ${mmr}, Gap: ${mmr}`)
+        .then((reply) => {
+            replyId = reply.id;
+            console.log('setting reply id:', replyId)
+        });
 }
 
 client.login(process.env.DISCORD_TOKEN);

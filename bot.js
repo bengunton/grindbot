8024b7699856ee0f1bcfa3d1c66929199b6987d1
Ben Gunton <bengunton@btinternet.com>
2022-03-25 19:30:37 +0000

    Add win loss counter

--- bot.js
+++ bot.js
@@ -8,10 +8,33 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+const prefix = "!";
+
+var wins = 0;
+var losses = 0;
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const messageContent = msg.content.slice(prefix.length).toLowerCase();
+
+    if (messageContent === 'ping') {
+        msg.reply('pong');
+    }
+
+    if (messageContent === 'win') {
+        wins++;
+        replyWinLoss(msg);
+    }
+
+    if (messageContent === 'loss') {
+        losses++;
+        replyWinLoss(msg);
+    }
 });
+
+replyWinLoss = (msg) => {
+    msg.reply(`Wins: ${wins}, Losses: ${losses}`)
+}
 
 client.login(process.env.DISCORD_TOKEN);
